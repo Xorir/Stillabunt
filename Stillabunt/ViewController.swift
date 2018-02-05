6f7b06d55a7c32fc58c8d358aab5ccbcef849748
Erman <xorir01@gmail.com>
2018-02-04 19:53:11 -0500

    location manager added to get current location

--- Stillabunt/ViewController.swift
+++ Stillabunt/ViewController.swift
@@ -7,19 +7,32 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    let locationManager = CLLocationManager()
+    var latitude: CLLocationDegrees? = nil
+    var longitude: CLLocationDegrees? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let locationCoordinates: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+        print("locations = \(locationCoordinates.latitude) \(locationCoordinates.longitude)")
+        latitude = locationCoordinates.latitude
+        longitude = locationCoordinates.longitude
+        locationManager.stopUpdatingLocation()
     }
-
-
 }
 
